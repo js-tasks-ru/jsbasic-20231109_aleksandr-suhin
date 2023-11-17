e0c880b2f8ed47a983cec12cf92eb011516761fc
Alexandr Sukhin <alexander.sukhin@beko.ru>
2023-11-17 11:01:00 +0300

    сделал все 5 1го модуля

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,10 @@
 function ucFirst(str) {
   // ваш код...
+  if (!Boolean(str)) return '';
+  if (str.legth === 1) return str.toUpperCase();
+  return str[0].toUpperCase() + str.slice(1);
 }
+
+// ucFirst('');
+// ucFirst('v');
+// ucFirst('vasya');
--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,12 @@
 function checkSpam(str) {
   // ваш код...
+  if (!Boolean(str)) return false;
+  return str.toLowerCase().includes('xxx') || str.toLowerCase().includes('1xbet');
 }
+
+// console.log(checkSpam(''));
+// console.log(checkSpam('xxx '));
+// console.log(checkSpam('fdxXx'));
+// console.log(checkSpam('f1xbetx'));
+// console.log(checkSpam('f1xbEtx'));
+// console.log(checkSpam('Hello'));
