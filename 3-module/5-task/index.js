3c2d7e3aa8893681165015d68408146ca4c35652
Alexandr Sukhin <alexander.sukhin@beko.ru>
2023-11-21 14:45:10 +0300

    сделал модуль 3

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
+  let numbers = str
+  .split(' ')
+  .filter(item=> isFinite(item));
+  return {
+    min: Math.min(...numbers),
+    max: Math.max(...numbers)
+  }  
 }
+
+// let inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+// console.log(getMinMax(inputData)); // { min: -5.8, max: 73  }
