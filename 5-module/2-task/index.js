9395c50bfee99da45a204cc3fdb328e7efb45583
Alexandr Sukhin <alexander.sukhin@beko.ru>
2023-12-04 14:48:28 +0300

    сделал 5й модуль (fix bug)

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,11 +1,9 @@
 function toggleText() {
   // ваш код...
-  document.addEventListener('click', function(event) {
-    if (event.target.classList.contains('toggle-text-button')){
-      // let elem = document.getElementById(id);
-      //text.hidden = !text.hidden;
+  document.addEventListener('click', function (event) {
+    if (event.target.classList.contains('toggle-text-button')) {
       if (text.hasAttribute('hidden')) text.removeAttribute('hidden');
-      else text.setAttribute('hidden',true);
+      else text.setAttribute('hidden', true);
     }
   });
 }
