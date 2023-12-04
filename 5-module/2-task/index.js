81514919b8d6328300290427855ca645e8dfca41
Alexandr Sukhin <alexander.sukhin@beko.ru>
2023-12-04 14:38:34 +0300

    сделал 5й модуль

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,11 @@
 function toggleText() {
   // ваш код...
+  document.addEventListener('click', function(event) {
+    if (event.target.classList.contains('toggle-text-button')){
+      // let elem = document.getElementById(id);
+      //text.hidden = !text.hidden;
+      if (text.hasAttribute('hidden')) text.removeAttribute('hidden');
+      else text.setAttribute('hidden',true);
+    }
+  });
 }
