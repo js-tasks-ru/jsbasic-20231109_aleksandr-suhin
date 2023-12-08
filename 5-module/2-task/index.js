3e319b35821ce1723f11002d879d48e4cb74dcca
Alexandr Sukhin <alexander.sukhin@beko.ru>
2023-12-08 08:15:22 +0300

    сделал 6й модуль, try pass tests

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,7 +1,8 @@
 function toggleText() {
   document.addEventListener('click', function (event) {
-    if (event.target != null && event.target.classList.contains('toggle-text-button')) {
-      event.target.nextElementSibling.hidden = !event.target.nextElementSibling.hidden;
+    if (event.target.classList.contains('toggle-text-button')) {
+      let text1 = event.target.nextElementSibling;
+      if (text1 === text) text1.hidden = !text1.hidden;
     }
   });
 }
