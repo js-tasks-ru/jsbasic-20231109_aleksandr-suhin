4d65d6198ecd2b0c75b08c5fde3ab8386f116f79
Alexandr Sukhin <alexander.sukhin@beko.ru>
2023-12-11 13:43:47 +0300

    6й модуль, пытаюсь пройти тест 5-module/2-task

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -2,7 +2,7 @@ function toggleText() {
   document.addEventListener('click', function (event) {
     if (event.target.classList.contains('toggle-text-button')) {
       let text1 = event.target.nextElementSibling;
-      if (text1 === text) text1.hidden = !text1.hidden;
+      text1.hidden = text1.hidden == true ? false : true;
     }
   });
 }
