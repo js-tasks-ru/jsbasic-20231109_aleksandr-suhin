6b1f134cadfca7ccca181dec685f191e0f8e9e7c
Alexandr Sukhin <alexander.sukhin@beko.ru>
2023-12-18 10:02:47 +0300

    сделал 7й модуль

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -3,5 +3,101 @@ import createElement from '../../assets/lib/create-element.js';
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.elem = this.#render(categories);
+
+    //обработаем клики
+    this.elem.addEventListener('click', (evt) => {
+      //клики по стрелам
+      if (evt.target.closest('.ribbon__arrow')) {
+        this.#OnArrowClick(evt);
+      }
+      if (evt.target.classList.contains('ribbon__item')) {
+        this.#OnLinkClick(evt);
+      }
+    });
+
+    //реализуем скрытие стрелок
+    this.elem.querySelector('.ribbon__inner').addEventListener('scroll', () => {
+      let ribbonInner = this.elem.querySelector('.ribbon__inner');
+      let left = this.elem.querySelector('.ribbon__arrow_left');
+      let right = this.elem.querySelector('.ribbon__arrow_right');
+
+      if (ribbonInner.scrollLeft === 0) left.classList.remove('ribbon__arrow_visible');
+      else left.classList.add('ribbon__arrow_visible');
+
+      let scrollWidth = ribbonInner.scrollWidth;
+      let scrollLeft = ribbonInner.scrollLeft;
+      let clientWidth = ribbonInner.clientWidth;
+      let scrollRight = scrollWidth - scrollLeft - clientWidth;
+      if (scrollRight < 1) right.classList.remove('ribbon__arrow_visible');
+      else right.classList.add('ribbon__arrow_visible');
+    });
   }
+
+  #render(categories) {
+    let template = `
+    <!--Корневой элемент RibbonMenu-->
+    <div class="ribbon">
+      <!--Кнопка прокрутки влево-->
+      <button class="ribbon__arrow ribbon__arrow_left">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+
+      <!--Ссылки на категории-->
+      <nav class="ribbon__inner">
+      `;
+    for (const category of categories) {
+      template += `<a href="#" class="ribbon__item" data-id="${category.id}">${category.name}</a>`;
+    }
+    template +=
+      `</nav>
+
+      <!--Кнопка прокрутки вправо-->
+      <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </button>
+    </div>
+    `;
+    return createElement(template);
+  }
+
+  #OnArrowClick(evt) {
+    let arrowButton = evt.target.closest('button');
+    //if (arrowbutton == null) return;
+    let isLeft = arrowButton.classList.contains('ribbon__arrow_left');
+    let isRight = arrowButton.classList.contains('ribbon__arrow_right');
+
+    //если кликнули по стрелке
+    if (isLeft || isRight) {
+      let delta = 350;
+      //если стрелка влево:
+      if (isLeft) delta = -delta;
+
+      //если стрелка вправо:
+      //if (isRight) ...
+
+      //для любой стрелки:
+      let ribbonInner = this.elem.querySelector('.ribbon__inner');
+      ribbonInner.scrollBy(delta, 0);
+    }
+  }
+
+  #OnLinkClick(evt) {
+    //снимем выделение со всех элементов
+    evt.target.parentElement.querySelectorAll('ribbon__item').forEach(element => {
+      element.classList.remove('ribbon__item_active');      
+    });
+    //и выставим только для нашего
+    evt.target.classList.add('ribbon__item_active');
+
+    //сгенерируем событие
+    let id = evt.target.dataset.id;
+    const customEvent = new CustomEvent('ribbon-select', {
+      detail: id,
+      bubbles: true,
+    })
+    this.elem.dispatchEvent(customEvent);
+  }
+
+
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,73 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
+  #elem
   constructor() {
+    this.#elem = this.#render();
+    this.#elem.querySelector('.modal__close').addEventListener('click', () => {
+      this.#closeSelf();
+    });
+    document.body.addEventListener('keydown', this.#OnEscapeDown);
+  }
+
+  open() {
+    if (!document.body.classList.contains('is-modal-open')) {
+      document.body.classList.add('is-modal-open');
+      document.body.appendChild(this.#elem);
+    }
+  }
+
+  setTitle(title) {
+    this.#elem.querySelector('.modal__title').innerHTML = title;
+  }
+
+  setBody(node) {
+    if (this.#elem === null) return;
+    //this.#elem.querySelector('.modal__body') = modalBody.replaceWith(node); - этот не проходит тесты
+    this.#elem.querySelector('.modal__body').innerHTML='';
+    this.#elem.querySelector('.modal__body').append(node);
+  }
+
+  #closeSelf() {
+    if (document.body.classList.contains('is-modal-open')) {
+      document.body.classList.remove('is-modal-open');
+      document.body.removeChild(this.#elem);
+      document.body.removeEventListener('keydown', this.#OnEscapeDown);
+    }
+  }
+
+  close() {
+    this.#closeSelf();
+  }
+
+  #OnEscapeDown = (event) => {
+    if (event.code == 'Escape') this.#closeSelf();
+  }
+
+  #render() {
+    let template = `
+    <div class="modal">
+      <!--Прозрачная подложка перекрывающая интерфейс-->
+      <div class="modal__overlay"></div>
+
+      <div class="modal__inner">
+        <div class="modal__header">
+          <!--Кнопка закрытия модального окна-->
+          <button type="button" class="modal__close">
+            <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+          </button>
+
+          <h3 class="modal__title">
+            Вот сюда нужно добавлять заголовок
+          </h3>
+        </div>
+
+        <div class="modal__body">
+          A сюда нужно добавлять содержимое тела модального окна
+        </div>
+      </div>    
+    </div>
+    `;
+    return createElement(template);
   }
 }
